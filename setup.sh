d4c8a8eb54284893a3ac51aaacf48dd280f0c685
root <root@localhost>
2024-06-03 10:15:32 +0200

    initial

---
+++ setup.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# Update package list and install packages
+sudo apt update
+sudo apt install -y console-setup unzip git neovim firefox-esr i3 dmenu fonts-roboto wget alacritty
+
+# Copy keyboard and console-setup files to /etc/default
+sudo cp keyboard /etc/default/keyboard
+sudo cp console-setup /etc/default/console-setup
+cp .Xresources ~/
+
+# Download and install RobotoMono Nerd Font
+wget -P ~/.local/share/fonts https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/RobotoMono.zip
+cd ~/.local/share/fonts
+unzip RobotoMono.zip
+rm RobotoMono.zip
+fc-cache -fv
+
+# Set timezone to Europe/Amsterdam
+sudo timedatectl set-timezone Europe/Amsterdam
+
+# Create src directory in the home directory and change to it
+mkdir -p ~/src
+cd ~/src
+
+# Download Emacs source
+wget -c https://ftpmirror.gnu.org/emacs/emacs-29.2.tar.gz
+
+# Install build dependencies for Emacs
+sudo apt build-dep emacs -y
+
+# Unpack the downloaded Emacs source
+tar -xzf emacs-29.2.tar.gz
+cd emacs-29.2
+
+# Configure, clean, and build Emacs
+./configure --with-native-compilation=aot --with-tree-sitter --with-gif --with-png --with-jpeg --with-rsvg --with-tiff --with-imagemagick --with-x-toolkit=lucid --with-json --with-mailutils
+make clean
+make -j8
