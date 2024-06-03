c9b69394a05ea9264efb6a6214c7ade4016098ee
root <root@localhost>
2024-06-03 10:39:29 +0200

    debug

--- setup.sh
+++ setup.sh
@@ -1,43 +1,42 @@
 #!/bin/bash
 
-# Create src directory in the home directory and change to it
+sudo apt update
+sudo apt upgrade -y
+sudo apt install -y git
+
 mkdir -p ~/src
 cd ~/src
 
-# Update package list and install packages
-sudo apt update
-sudo apt install -y console-setup unzip git neovim firefox-esr i3 dmenu fonts-roboto wget alacritty
+git clone https://github.com/0xcefaedfe/dotfiles
+cd dotfiles
 
-# Copy keyboard and console-setup files to /etc/default
 sudo cp keyboard /etc/default/keyboard
 sudo cp console-setup /etc/default/console-setup
 cp Xresources ~/.Xresources
+exit 0
 
-# Download and install RobotoMono Nerd Font
+sudo apt install -y console-setup unzip git neovim firefox-esr i3 dmenu fonts-roboto wget alacritty
+
 wget -P ~/.local/share/fonts https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/RobotoMono.zip
 cd ~/.local/share/fonts
 unzip RobotoMono.zip
 rm RobotoMono.zip
 fc-cache -fv
 
-# Set timezone to Europe/Amsterdam
 sudo timedatectl set-timezone Europe/Amsterdam
 
-
-# Download Emacs source
 wget -c https://ftpmirror.gnu.org/emacs/emacs-29.3.tar.gz
 
-# Install build dependencies for Emacs
 sudo apt build-dep emacs -y
 
-# Unpack the downloaded Emacs source
 tar -xzf emacs-29.3.tar.gz
 cd emacs-29.3
 
-# Configure, clean, and build Emacs
 ./configure --with-native-compilation=aot --with-tree-sitter --with-gif --with-png --with-jpeg --with-rsvg --with-tiff --with-imagemagick --with-x-toolkit=lucid --with-json --with-mailutils
 make clean
 make -j8
 
-# Clean up
-rm -rf ~/src/emacs-29.2
+cd ~
+
+rm -rf ~/src/emacs-29.3
+rm -rf ~/src/dotfiles-main
