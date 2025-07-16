4cd1d985176fd3a54d8f509e9876c8ce77bbdec4
T <t@nxtsqr.com>
2025-07-16 10:02:59 +0200

    early init and theme update

--- emacs/early-init.el
+++ emacs/early-init.el
@@ -1,1 +1,3 @@
-(add-to-list 'default-frame-alist '(background-color . "#212121"))
+(add-to-list 'default-frame-alist '(foreground-color . "#e4e4ef"))
+(add-to-list 'default-frame-alist '(background-color . "#282828"))
+(add-to-list 'default-frame-alist '(border-color . "#3d3532"))
